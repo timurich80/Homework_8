002c661704d9d6040f88f52e8bcb7ded7791fa3e
timurich80 <timurich80@gmail.com>
2022-09-28 19:22:25 +0300

    начал и не пнимаю что в чем

---
+++ homework3/Program.cs
@@ -0,0 +1,46 @@
+﻿/* 
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 
+*/
+int i = ReadInt("Введите колличество строк: ");
+int j = ReadInt("Введите колличество столбцов: ");
+
+int[,] firstArray = new int[i,j];
+int[,] secondArray = new int[j,j];
+int[,] resultArray = new int[i,j];
+
+FillArray(firstArray)
+FillArray(secondArray)
+PrintArray(firstArray)
+PrintArray(secondArray)
+PrintArray(resultArray)
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] =new Random().Next (1,10);
+        }
+    }
+}
+
+void PrintArray( int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] =new Random().Next (1,10);
+        }
+    }
+System.Console.WriteLine();
+}
+
