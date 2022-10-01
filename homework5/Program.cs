fd15ecbcf6cbcafee21011784b9c9c47b14331fe
timurich80 <timurich80@gmail.com>
2022-10-01 15:57:31 +0300

    код работатет

--- homework5/Program.cs
+++ homework5/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07 
+*/
+
+
+
+
+int [,] spiralMatrix = new int [4,4];
+FillArray(spiralMatrix);
+System.Console.WriteLine();
+PrintArray(spiralMatrix);
+
+
+
+void PrintArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i,j] / 10 <= 0)
+        Console.Write($" {array[i,j]} ");
+
+        else Console.Write($"{array[i,j]} ");
+    }
+System.Console.WriteLine();
+}
+}
+
+void FillArray(int[,] array)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    int n = array.GetLength(0);
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+
+
